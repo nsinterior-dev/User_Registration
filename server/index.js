4067a7849c6b2746bf764d54d2c46cf4aad28d3d
NicolleInterior <intercoeli@gmail.com>
2021-09-11 11:40:59 +0800

    changed the mongodb link

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const userModel = require('./models/User');
 const app = express();
 const PORT = 4000;
 
-mongoose.connect('mongodb+srv://adminUser:password1234@cluster0.lkzei.mongodb.net/user?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://127.0.0.1:27017/user', {
     useNewUrlParser: true,
     
 });
